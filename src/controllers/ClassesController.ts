4ed4e67639b103190ed8ed1ab0552c0ea6f70f34
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-08-04 09:41:24 -0300

    feat: list classes

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -10,7 +10,41 @@ interface ScheduleItem {
   to: string;
 }
 
+interface IndexRequestQuery {
+  subject: string;
+  week_day: string;
+  time: string;
+  [key: string]: any;
+}
+
 class ClassesController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { subject, week_day, time } = request.query as IndexRequestQuery;
+
+    if (!subject || !week_day || !time) {
+      return response.status(400).json({
+        error: "Está faltando os filtros"
+      });
+    }
+
+    const timeInMinutes = convertHoursToMinutes(time);
+
+    const classes = await db('classes')
+      .whereExists(function () {
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+      })
+      .where('classes.subject', '=', subject)
+      .join('users', 'classes.user_id', '=', 'users.id')
+      .select(['classes.*', 'users.*']);
+
+    return response.status(200).json(classes);
+  }
+
   public async store(request: Request, response: Response): Promise<Response> {
     const {
       name,
--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ const classesController = new ClassesController();
 
 const routes = Router();
 
+routes.get('/classes', classesController.index);
 routes.post('/users', classesController.store);
 
 export default routes;
